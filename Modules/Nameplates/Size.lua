32e119f26332fd233015a8ecebb61f7223174669
Eleum <wotlk777@gmail.com>
2026-02-27 21:16:56 +0100

    use deterministic sizing callback

--- Modules/Nameplates/Size.lua
+++ Modules/Nameplates/Size.lua
@@ -3,50 +3,16 @@ local Size = avUI:NewModule("avUI.Nameplates.Size", "AceHook-3.0", "AceEvent-3.0
 function Size:OnInitialize()
 end
 
-local function AdjustNameplateSize(nameplate)
-    if not nameplate then
-        return
-    end
-
-    local frame = nameplate.UnitFrame and nameplate.UnitFrame.HealthBarsContainer
-
-    if not frame or frame:IsForbidden() then
+local function SetHealthBarHeight(frame)
+    if not frame or not frame.healthBar then
         return
     end
 
     frame:SetHeight(10)
 end
 
-local function AdjustNameplateSizeUnit(unit)
-    local nameplate = nameplate or C_NamePlate.GetNamePlateForUnit(unit)
-
-    if not nameplate then
-        return
-    end
-
-    AdjustNameplateSize(nameplate)
-end
-
-local function AdjustAllNameplateSizes()
-    local nameplates = C_NamePlate.GetNamePlates()
-
-    for _, nameplate in ipairs(nameplates) do
-        AdjustNameplateSize(nameplate)
-    end
-end
-
-local function OnNameplateAdded(event, unit)
-    AdjustNameplateSizeUnit(unit)
-end
-
-local function OnUpdateAnchors()
-    AdjustAllNameplateSizes()
-end
-
 function Size:OnEnable()
-    self:RegisterEvent("NAME_PLATE_UNIT_ADDED", OnNameplateAdded)
-
-    self:SecureHook(NamePlateUnitFrameMixin, "UpdateAnchors", OnUpdateAnchors)
+    self:SecureHook(PixelUtil, "SetHeight", SetHealthBarHeight)
 end
 
 function Size:OnDisable()
