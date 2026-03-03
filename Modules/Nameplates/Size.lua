daab381f8fc409a87ed847d555a44e7eaa829cba
Eleum <wotlk777@gmail.com>
2026-03-03 20:48:01 +0100

    remove debug stuff

--- Modules/Nameplates/Size.lua
+++ Modules/Nameplates/Size.lua
@@ -3,7 +3,7 @@ local Size = avUI:NewModule("avUI.Nameplates.Size", "AceHook-3.0", "AceEvent-3.0
 function Size:OnInitialize()
 end
 
-local function SetHealthBarHeight(frame, a)
+local function SetHealthBarHeight(frame)
     if not frame or not frame.healthBar or frame:IsForbidden() then
         return
     end
