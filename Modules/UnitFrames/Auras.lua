7ce3037b0c506d2c30781260c27bd894ba580589
Eleum <wotlk777@gmail.com>
2026-07-29 03:03:18 +0200

    attempt at fixing atonement aura

--- Modules/UnitFrames/Auras.lua
+++ Modules/UnitFrames/Auras.lua
@@ -176,13 +176,171 @@ local function ResetRenewingMistAura(frame)
 end
 
 function Auras:OnEnable()
-    self:RegisterEvent("UNIT_AURA", function(event, unit, auras)
-        ApplyAtonementAura(event, unit, auras)
-        ApplyRenewingMistAura(event, unit, auras)
-    end)
+    local function CreateFrameAuraContainer(frame)
+        local container = CreateFrame("AuraContainer", nil, frame, "CustomAuraContainerTemplate")
+
+        container:SetSize(1, 1)
+        container:SetPoint("CENTER", frame, "CENTER", 0, 0)
+
+        return container
+    end
+
+    local function InitializeFrame(frame)
+        local function AddParentFrameNameComponent(frame, parentFrame)
+            local name = parentFrame.GetName and parentFrame:GetName()
+
+            if name then
+                local fsName = _G[name .. "Name"]
+
+                if fsName and fsName.IsShown and fsName:IsShown() then
+                    local font, size, flags = fsName:GetFont()
+                    local text = fsName.GetText and fsName:GetText() or ""
+
+                    local fs = frame:CreateFontString(nil, "ARTWORK", "GameFontDisable")
+
+                    fs:SetText(text)
+                    fs:SetWordWrap(false)
+                    fs:SetFont(font, size, "OUTLINE")
+                    fs:SetTextColor(unpack(HexToRGB("#ffd444")))
+                    fs:SetWidth(fsName:GetWidth())
+                    fs:SetPoint("TOPLEFT", fsName, "TOPLEFT")
+
+                    if fsName.GetJustifyH and fsName.SetJustifyH then
+                        fs:SetJustifyH(fsName:GetJustifyH())
+                    end
+
+                    if fsName.GetJustifyV and fsName.SetJustifyV then
+                        fs:SetJustifyV(fsName:GetJustifyV())
+                    end
+
+                    return fs
+                end
+            end
+
+            return nil
+        end
+
+        local function AddAuraIconComponent(frame)
+            local icon = frame:CreateTexture(nil, "ARTWORK")
+            icon:SetAllPoints(frame)
+            icon:SetAlpha(0)
+
+            frame:SetIcon(icon)
+            frame:SetSize(32, 32)
+            frame:SetPoint("CENTER")
+            frame:EnableMouse(false)
+        end
+
+        local function AddStatusBarComponent(frame, anchorFrame, color)
+            local bar = CreateFrame("StatusBar", nil, frame)
+
+            bar:SetStatusBarTexture("Interface\\Buttons\\WHITE8x8")
+            bar:GetStatusBarTexture():SetVertexColor(anchorFrame:GetTextColor())
+
+            local height = 1
+
+            bar:SetHeight(height)
+            bar:SetPoint("TOP", anchorFrame, "BOTTOM", 0, -height)
+            bar:SetPoint("LEFT", anchorFrame, "LEFT")
+            bar:SetPoint("RIGHT", anchorFrame, "RIGHT")
+            bar:SetFrameLevel(frame:GetFrameLevel())
+
+            local options = {}
+
+            if Enum.StatusBarInterpolation then
+                options.interpolation = Enum.StatusBarInterpolation.Immediate
+            end
+
+            if Enum.StatusBarTimerDirection then
+                options.direction = Enum.StatusBarTimerDirection.RemainingTime
+            end
+
+            frame:SetDurationBar(bar, options)
+        end
+
+        local container = frame.GetParent and frame:GetParent()
+        local parent = container and container.GetParent and container:GetParent()
+
+        if not parent then
+            return
+        end
+
+        local fs = AddParentFrameNameComponent(frame, parent)
+        -- AddAuraIconComponent(frame)
+
+        if fs then
+            AddStatusBarComponent(frame, fs, fs:GetTextColor())
+        end
+
+        -- frame:SetSize(32, 32)
+
+        -- frame.bg = frame:CreateTexture(nil, "BACKGROUND", nil, -8)
+        -- frame.bg:SetAllPoints(frame)
+        -- frame.bg:SetColorTexture(1, .83, .27, .5)
+        -- frame.bg:SetBlendMode("ADD")
+        -- frame.bg:SetBlendMode("MOD")
+        -- frame.bg:SetBlendMode("BLEND")
+
+        -- frame.cooldown = CreateFrame("Cooldown", nil, frame, "CooldownFrameTemplate")
+        -- frame.cooldown:SetAllPoints(frame)
+        -- frame:SetDurationCooldown(frame.cooldown)
+
+        -- if frame.SetHideTooltipInCombat then
+        --     frame:SetHideTooltipInCombat(true)
+        -- end
+    end
+
+    local function CreateAuraContainerSlot()
+        local key = "slot1"
+        local filter = "HELPFUL|PLAYER"
+        local options = {
+            candidateFilters = {
+                includeSpellIDs = {
+                    [194384] = true
+                }
+            },
+            initializeFrame = InitializeFrame
+        }
+
+        return {key, filter, options}
+    end
+
+    local function CreateAuraContainer(frame)
+        if not frame then
+            return
+        end
+
+        local container = frame.__avuiAuraContainer
+
+        if container then
+            container:SetParent(nil)
+            container:Hide()
+            frame.__avuiAuraContainer = nil
+        end
+
+        if not frame.IsForbidden or frame:IsForbidden() then
+            return
+        end
+
+        local unit = UnitFrames:GetFrameUnit(frame)
+
+        if not unit or not UnitFrames:IsPartyOrRaidUnit(unit) then
+            return
+        end
+
+        local container = CreateFrameAuraContainer(frame)
+
+        local slot = CreateAuraContainerSlot()
+        container:AddAuraSlot(unpack(slot))
+
+        container:SetUnit(unit)
+        container:UpdateAllAuras()
+
+        frame.__avuiAuraContainer = container
+    end
+
     self:SecureHook("CompactUnitFrame_SetUnit", function(frame)
-        ResetAtonementAura(frame)
-        ResetRenewingMistAura(frame)
+        CreateAuraContainer(frame)
     end)
 end
 
